e01cc699239d8ee0f714bc8ac4403d3bce0c8a3a
Rahman <rahmanmuti@gmail.com>
2020-06-07 21:34:34 +0530

    data base connected

--- src/main/java/technicalblog/service/PostService.java
+++ src/main/java/technicalblog/service/PostService.java
@@ -3,6 +3,7 @@ package technicalblog.service;
 import org.springframework.stereotype.Service;
 import technicalblog.model.Post;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -16,27 +17,43 @@ public class PostService {
     public ArrayList<Post> getAllPosts() {
 
 
-
+//
         ArrayList<Post> posts = new ArrayList<>();
+//
+//        Post post1 = new Post();
+//        post1.setTitle("Post 1");
+//        post1.setBody("Post Body 1");
+//        post1.setDate(new Date());
+//
+//        Post post2 = new Post();
+//        post2.setTitle("Post 2");
+//        post2.setBody("Post Body 2");
+//        post2.setDate(new Date());
+//
+//        Post post3 = new Post();
+//        post3.setTitle("Post 3");
+//        post3.setBody("Post Body 3");
+//        post3.setDate(new Date());
+//
+//        posts.add(post1);
+//        posts.add(post2);
+//        posts.add(post3);
 
-        Post post1 = new Post();
-        post1.setTitle("Post 1");
-        post1.setBody("Post Body 1");
-        post1.setDate(new Date());
+        try{
+            Class.forName("org.postgresql.Driver");
 
-        Post post2 = new Post();
-        post2.setTitle("Post 2");
-        post2.setBody("Post Body 2");
-        post2.setDate(new Date());
-
-        Post post3 = new Post();
-        post3.setTitle("Post 3");
-        post3.setBody("Post Body 3");
-        post3.setDate(new Date());
-
-        posts.add(post1);
-        posts.add(post2);
-        posts.add(post3);
+            Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/technicalblog","postgres", "myself");
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM thirdtime");
+            while(rs.next()){
+                Post post = new Post();
+                post.setTitle(rs.getString("title"));
+                post.setBody(rs.getString("body"));
+                posts.add(post);
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
 
         return posts;
     }
