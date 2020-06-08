4e5559e6671a50e1fa1c89940d4a158880c1b781
Rahman <rahmanmuti@gmail.com>
2020-06-08 07:10:12 +0530

    JPA implementation

--- src/main/java/technicalblog/service/PostService.java
+++ src/main/java/technicalblog/service/PostService.java
@@ -3,9 +3,13 @@ package technicalblog.service;
 import org.springframework.stereotype.Service;
 import technicalblog.model.Post;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import javax.persistence.TypedQuery;
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.List;
 
 @Service
 public class PostService {
@@ -14,7 +18,9 @@ public class PostService {
         System.out.println("*** PostService ***");
     }
 
-    public ArrayList<Post> getAllPosts() {
+    @PersistenceUnit(unitName = "techblog")
+    private EntityManagerFactory emf;
+    public List<Post> getAllPosts() {
 
 
 //
@@ -39,30 +45,34 @@ public class PostService {
 //        posts.add(post2);
 //        posts.add(post3);
 
-        Connection connection=null;
-        try{
-            Class.forName("org.postgresql.Driver");
+        EntityManager em = emf.createEntityManager();
+        TypedQuery<Post> query = em.createQuery("SELECT p from Post p", Post.class);
+        List<Post> resultList = query.getResultList();
 
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/technicalblog","postgres", "myself");
-            Statement statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery("SELECT * FROM thirdtime");
-            while(rs.next()){
-                Post post = new Post();
-                post.setTitle(rs.getString("title"));
-                post.setBody(rs.getString("body"));
-                posts.add(post);
-            }
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }finally{
-            try{
-                connection.close();
-            }catch(SQLException e){
-                e.printStackTrace();
-            }
-        }
+//        Connection connection=null;
+//        try{
+//            Class.forName("org.postgresql.Driver");
+//
+//            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/technicalblog","postgres", "myself");
+//            Statement statement = connection.createStatement();
+//            ResultSet rs = statement.executeQuery("SELECT * FROM thirdtime");
+//            while(rs.next()){
+//                Post post = new Post();
+//                post.setTitle(rs.getString("title"));
+//                post.setBody(rs.getString("body"));
+//                posts.add(post);
+//            }
+//        } catch (ClassNotFoundException | SQLException e) {
+//            e.printStackTrace();
+//        }finally{
+//            try{
+//                connection.close();
+//            }catch(SQLException e){
+//                e.printStackTrace();
+//            }
+//        }
 
-        return posts;
+        return resultList;
     }
 
     public ArrayList<Post> getOnePost() {
